80a1010d78cb7cbd196eca18d7a7ade130c2c6c8
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2025-01-16 19:21:10 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import pages from 'vite-plugin-pages';
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [
-    react(),
-    pages(),
-  ],
+  plugins: [react()],
 })
